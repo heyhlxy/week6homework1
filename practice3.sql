1fb46e9b675f8079331d992d6fb0b0c9b213e65e
heyhlxy <heyhlxy@163.com>
2019-07-05 21:30:23 +0800

    practice3 completed

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,14 @@
 #取得平均薪水最高的公司
 #输出公司名称和平均薪水：companyName avgSalary
 
-
+SELECT companyName,avgSalary
+FROM(
+  SELECT companyId,avgSalary
+  FROM(
+    SELECT AVG(salary) AS avgSalary,companyId
+    FROM employee
+    GROUP BY companyId
+  )AS comAvgSalary
+  ORDER BY avgSalary DESC
+  LIMIT 1) AS maxAvgSalary,company
+WHERE company.id=maxAvgSalary.companyId;
